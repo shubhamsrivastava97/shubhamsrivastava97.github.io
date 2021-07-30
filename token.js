fe1e21119a086369e1e07015a7d314ca31699db2
ORADEV\shubhasr <shubham.ss.srivastava@oracle.com>
2021-07-30 11:34:50 +0530

    Changed getToken request body

--- token.js
+++ token.js
@@ -5,13 +5,13 @@ const code = urlParams.get('code');
 const getToken = () => {
     const url = "https://oauth2.googleapis.com/token";
     const body = {
-        code: code,
-        redirect_uri: "https://shubhamsrivastava97.github.io/product",
-        client_id: "554423675989-u38fd4g646q444j9kdrcr0r4k35bhdp9.apps.googleusercontent.com",
-        client_secret: "40w0RT9zORmQY1YzIm4p-Gl5",
-        scope: "https://www.googleapis.com/auth/content",
-        grant_type: "authorization_code"
-    };
+        "code": code,
+        "redirect_uri": "https://shubhamsrivastava97.github.io/product",
+        "client_id": "554423675989-u38fd4g646q444j9kdrcr0r4k35bhdp9.apps.googleusercontent.com",
+        "client_secret": "40w0RT9zORmQY1YzIm4p-Gl5",
+        "scope": "https://www.googleapis.com/auth/content",
+        "grant_type": "authorization_code"
+    }
     axios({
         method: 'post',
         url: url,
