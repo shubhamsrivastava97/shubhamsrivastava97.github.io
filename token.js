7f5a462e47aa28da5efc73a50291b55a0d057ec1
ORADEV\shubhasr <shubham.ss.srivastava@oracle.com>
2021-07-30 10:45:17 +0530

    Modified redirect url

--- token.js
+++ token.js
@@ -1,12 +1,12 @@
 
-const urlParams = window.location.search;
+const urlParams = new URLSearchParams(window.location.search);
 const code = urlParams.get('code');
 
 const getToken = () => {
     const url = "https://oauth2.googleapis.com/token";
     const body = {
         code: code,
-        redirect_uri: "https://shubhamsrivastava97.github.io/",
+        redirect_uri: "https://shubhamsrivastava97.github.io/product",
         client_id: "554423675989-u38fd4g646q444j9kdrcr0r4k35bhdp9.apps.googleusercontent.com",
         client_secret: "40w0RT9zORmQY1YzIm4p-Gl5",
         scope: "https://www.googleapis.com/auth/content",
