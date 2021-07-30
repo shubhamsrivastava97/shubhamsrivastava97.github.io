48678285c6708d7587505bb35712a1c8e37e7834
ORADEV\shubhasr <shubham.ss.srivastava@oracle.com>
2021-07-30 13:31:15 +0530

    Added api call for product list

--- token.js
+++ token.js
@@ -1,6 +1,8 @@
 
 const urlParams = new URLSearchParams(window.location.search);
 const code = urlParams.get('code');
+let access_token;
+let refresh_token;
 
 const getToken = () => {
     const url = "https://oauth2.googleapis.com/token";
@@ -17,23 +19,34 @@ const getToken = () => {
         url: url,
         data: body
     })
-        .then(access)
+        .then(getMerchantId)
         .catch(err => console.log(err.response));
 }
 
-function access(response) {
+function getMerchantId(response) {
     console.log(response);
-    const access_token = response.data.access_token;
-    const refresh_token = response.data.refresh_token;
+    access_token = response.data.access_token;
+    refresh_token = response.data.refresh_token;
     const merchantapi = "https://www.googleapis.com/content/v2/accounts/authinfo";
     axios.get(merchantapi, {
         headers: {"Authorization": `Bearer ${access_token}`}
     })
+        .then(getProductList)
+        .catch(err => console.log(err.data));
+}
+
+function getProductList(response) {
+    console.log(response);
+    const merchantId = res.data.accountIdentifiers[0].merchantId;
+    document.getElementById("accinfo").innerText = merchantId;
+    const productapi = `https://www.googleapis.com/content/v2/${merchantId}/products`;
+    axios.get(productapi, {
+        headers: {"Authorization": `Bearer ${access_token}`}
+    })
         .then(res => {
             console.log(res.data);
-            document.getElementById("accinfo").innerText = res.data.accountIdentifiers[0].merchantId;
+            document.getElementById("prodinfo").innerHTML = res.data.resources;
         })
-        .catch(err => console.log(err.data));
 }
 
 getToken();
